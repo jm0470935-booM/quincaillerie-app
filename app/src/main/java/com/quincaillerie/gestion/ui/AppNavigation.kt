3646f3ee0bc8f31e2a8de685907e80e9df56645e
jm0470935-booM <jm0470935@gmail.com>
2026-08-15 18:28:37 +0200

    Update AppNavigation.kt

--- app/src/main/java/com/quincaillerie/gestion/ui/AppNavigation.kt
+++ app/src/main/java/com/quincaillerie/gestion/ui/AppNavigation.kt
@@ -13,6 +13,7 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import androidx.compose.foundation.layout.padding
 import com.quincaillerie.gestion.ui.screens.DashboardScreen
 import com.quincaillerie.gestion.ui.screens.ProductListScreen
 import com.quincaillerie.gestion.ui.screens.SaleScreen
